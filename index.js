f9f0b653ab6e7a16dd2c7c4ab182c26ab4a5e4c6
Mbah Elvis Ndum <130441973+Elvisco1989@users.noreply.github.com>
2024-04-29 09:23:06 +0200

    I add properties and methods

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
  
+ const baseUrl = "https://localhost:7299/api/Musics"
+ Vue.CreateApp({
+    data(){
+        return{
+            Id: null,
+            Title:"",
+            Artist: "",
+            PublicationYear:null,
+            Duration: null,
+
+            Music : {
+            Id: null,
+            Title:"",
+            Artist: "",
+            PublicationYear:null,
+            Duration: null,
+
+            },
+
+            Musics : []
+
+        }
+    },
+    methods: {
+        async getMusics() {
+            try {
+
+                const respond = await axios.get(baseUrl)
+               
+                
+                this.Musics = await respond.data
+                // console.log(Cars)
+            }
+            catch (ex) {
+                alert(ex.message)
+            }
+
+        },
+
+    } 
+ }).mount("#app")
