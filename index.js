aa71b54f2a858a1f22e2671282578b155d5fff67
Mbah Elvis Ndum <130441973+Elvisco1989@users.noreply.github.com>
2024-05-02 13:00:11 +0200

    I added Get by Id and delete functions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
  
- const baseUrl = "https://localhost:7299/api/Musics"
+//  const baseUrl = "https://localhost:7299/api/Musics"
+ const baseUrl = "https://drrest20240418140840.azurewebsites.net/api/Musics"
  Vue.createApp({
     data(){
         return{
@@ -18,7 +19,9 @@
 
             },
 
-            Musics : []
+            Musics : [],
+            Singlemusic : null,
+            deletemessage: ""
 
         }
     },
@@ -37,6 +40,28 @@
             }
 
         },
+        async getbyId(id){
+            const url = baseUrl + "/" + id
+            try{
+                const respond = await axios.get(url)
+                this.Singlemusic = await respond.data
+            }
+            catch(ex){
+                alert(ex.message)
+            }
+        },
+        async deleteMusic(deleteId){
+            const url = baseUrl + "/" + deleteId
+            try{
+                respond = await axios.delete(url)
+                this.deletemessage = respond.status + "" + respond.statusText
+                this.getMusics()
+            }
+            catch(ex){
+                alert(ex.message)
+            }
+        }
+        
 
     } 
  }).mount("#app")
